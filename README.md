99d7f73e8d634fc65ef70be1d7adf4f8e8b44c7c
patka <plakato01@gmail.com>
2018-09-23 23:21:25 +0200

    first version of the page

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Stránka na cvičenie programovania I pre matematikov
 =====================================================
-
+development:
 `yarn run watch-css`
+deploy:
+`yarn run deploy`
---
+++ deploy.js
@@ -0,0 +1,45 @@
+// copy dist files
+const fs = require('fs-extra');
+ 
+if( fs.existsSync('dist') ){
+  //fs.removeSync('dist');
+}
+const files_and_folders_to_deploy = ["index.html", "images", "css"];
+
+const files_copying = files_and_folders_to_deploy.map(f => fs.copy(f, 'dist/'+f));
+
+Promise.all(files_copying).then(
+  () => {
+    // config with ftp settings
+    const configFile = require('./config/ftp_credentials.json');
+
+    console.log(__dirname + '/dist');
+
+    var FtpDeploy = require('ftp-deploy');
+    var ftpDeploy = new FtpDeploy();
+     
+    var config = {
+        user: configFile.user,                   
+        password: configFile.password,           
+        host: configFile.host,
+        port: configFile.port,
+        localRoot: __dirname + '/dist',
+        remoteRoot: configFile.path,
+        include: ['*', '**/*'],
+        exclude: [],
+        deleteRemote: true              // delete existing files at destination before uploading
+    }
+
+ 
+     
+    // use with promises
+    ftpDeploy.deploy(config)
+        .then(res => console.log('finished'))
+        .catch(err => console.log(err))
+
+    
+    
+  },
+  (err) => console.log(err) 
+);
+
